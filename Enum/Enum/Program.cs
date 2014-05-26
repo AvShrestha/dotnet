ea082727f37156bd424b06be9f33574f29547270
av shrestha <shrestha_4me@hotmail.com>
2014-05-26 07:54:56 +0545

    added comment new file added

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnumApplication
+{
+    class Program
+    {
+        
+            enum Days {sun, Mon, Tue, Wed, Thu, Fri, sat};
+            static void Main(string[] args)
+            {
+            int WeekdayStart = (int)Days.Mon;
+            int WeekdayEnd = (int)Days.Fri;
+            Console.WriteLine("Monday :{0}", WeekdayStart);
+            Console.WriteLine("Friday :{0}",WeekdayEnd);
+            Console.ReadKey();
+            }
+        }
+    
+}
